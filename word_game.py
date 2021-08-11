0502e621b9d220f7252b59492f18ff0e8b80aedc
ValMarie <valanthonio_marie@yahoo.com>
2021-08-11 18:07:54 +0100

    updated hangman

--- word_game.py
+++ word_game.py
@@ -13,29 +13,44 @@ def get_valid_wordlist(words):
 def wordGame():
     word = get_valid_wordlist(words)
     letters = set(word)
-    each_alphabet = set(string.ascii_uppercase)
+    each_alphabet = set(string.ascii_lowercase)
     used_letters = set()
 
-    while len(letters) > 0:
+    no_of_trials = 4
+
+    while len(letters) > 0 and no_of_trials > 0:
         # getting the used letters
-        print("You have used these letters: ", " ".join(used_letters)) 
+        print("You have ", no_of_trials, "lives left and You have used these letters: ", " ".join(used_letters)) 
 
         # showing the current word
         current_word = [letter if letter in used_letters else "-" for letter in word]
         print("Current word: ", " ".join(current_word))
 
-        inputted_letters = input("Guess the letter: ").upper()
+        inputted_letters = input("Guess the letter: ")
         if inputted_letters in each_alphabet - used_letters:
             used_letters.add(inputted_letters)
             if inputted_letters in letters:
                 letters.remove(inputted_letters)
+            else:
+                no_of_trials = no_of_trials - 1
+                print("Sorry, that letter ", inputted_letters, " is not in word")
         
         elif inputted_letters in used_letters:
-            print("You already used that leter")
+            print("You already used that letter")
 
         else:
             print("Invalid entry. Try again")
+        print("\n") 
 
+    if no_of_trials == 0:
+        print("GAME OVER \n Sorry the word is", word)
+    else:
+        print("Good job!\n You correctly guesed the word", word)
+    
+        
+        
+
+player_input = print("Guess the  word: ")
 wordGame()
-# player_input = input("Guess the  word: ")
+
 
